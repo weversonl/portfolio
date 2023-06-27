037520abe8e9aaf5daea2be883ed2db1e9af5b47
WeversonL <weversonlemos10@protonmail.com>
2023-06-27 17:33:20 -0300

    feat: Adjust for small screens

--- src/components/TechnologiesContainer.jsx
+++ src/components/TechnologiesContainer.jsx
@@ -13,12 +13,12 @@ import {
 import "../styles/components/technologiescontainer.sass";
 
 const technologies = [
-  { id: "java", name: "Java", icon: <DiJava />, description: "Desenvolvo em Java há 2 anos. Experiência sólida no desenvolvimento de API's" },
-  { id: "spring", name: "SpringBoot", icon: <SiSpring />, description: "Utilizo SpringBoot a pelo menos 2 anos, para desenvolvimento de REST e GraphQL API's" },
-  { id: "docker", name: "Docker", icon: <SiDocker />, description: "Utilizei Docker para aprimorar ambientes de CI/CD e garantir melhor qualidade no desenvolvimento" },
-  { id: "linux", name: "Linux", icon: <DiLinux />, description: "Linux é meu sistema operacional principal e estudo constantemente para aprimorar meus conhecimentos nele" },
-  { id: "psql", name: "PostgreSQL", icon: <SiPostgresql />, description: "Utilizei amplamente o PostgreSQL como uma opção SQL em muitos dos projetos que desenvolvi" },
-  { id: "mongo", name: "MongoDB", icon: <SiMongodb />, description: "Em outros projetos, optei por bancos de dados NoSQL, como o MongoDB, para atender às necessidades específicas" }
+  { id: "java", name: "Java", icon: <DiJava className="icon-tech" />, description: "Desenvolvo em Java há 2 anos. Experiência sólida no desenvolvimento de API's" },
+  { id: "spring", name: "SpringBoot", icon: <SiSpring className="icon-tech" />, description: "Utilizo SpringBoot a pelo menos 2 anos, para desenvolvimento de REST e GraphQL API's" },
+  { id: "docker", name: "Docker", icon: <SiDocker className="icon-tech" />, description: "Utilizei Docker para aprimorar ambientes de CI/CD e garantir melhor qualidade no desenvolvimento" },
+  { id: "linux", name: "Linux", icon: <DiLinux className="icon-tech" />, description: "Linux é meu sistema operacional principal e estudo constantemente para aprimorar meus conhecimentos nele" },
+  { id: "psql", name: "PostgreSQL", icon: <SiPostgresql className="icon-tech" />, description: "Utilizei amplamente o PostgreSQL como uma opção SQL em muitos dos projetos que desenvolvi" },
+  { id: "mongo", name: "MongoDB", icon: <SiMongodb className="icon-tech" />, description: "Em outros projetos, optei por bancos de dados NoSQL, como o MongoDB, para atender às necessidades específicas" }
 ];
 
 const TechnologiesContainer = () => {
