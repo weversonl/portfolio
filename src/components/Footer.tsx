eb17fe520763363ed2a14dae90bd076982e73b2f
weversonl <weversonl@proton.me>
2026-02-05 13:57:54 -0300

    refactor: colorschema e theming

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import SocialNetworkContainer from "./SocialNetworkContainer";
 
 const Footer = () => {
   return (
-    <footer className="w-full py-8 mt-20">
+    <footer className="w-full pt-8 pb-20 sm:pb-24 mt-20">
       <div className="container mx-auto px-6">
         <div className="flex flex-col md:flex-row items-center justify-between gap-4">
           <p className="text-slate-400 text-sm">
